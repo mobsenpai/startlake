874cea48178bf49af6996519ffaedbc6f995890c
mobsenpai <work.velocity806@passinbox.com>
2025-03-08 17:45:03 +0530

    changes

--- index.js
+++ index.js
@@ -1,137 +1,137 @@
-import { weatherCodes } from './weatherCodes.js'
+import { weatherCodes } from "./weatherCodes.js";
 
-document.addEventListener('DOMContentLoaded', () => {
-	const settingsForm = document.getElementById('settings-form')
-	const toggleSettingsButton = document.getElementById('toggle-settings')
+document.addEventListener("DOMContentLoaded", () => {
+  const settingsForm = document.getElementById("settings-form");
+  const toggleSettingsButton = document.getElementById("toggle-settings");
 
-	const settings = JSON.parse(localStorage.getItem('settings'))
+  const settings = JSON.parse(localStorage.getItem("settings"));
 
-	if (!settings) {
-		settingsForm.style.display = 'block'
-	} else {
-		settingsForm.style.display = 'none'
-		toggleSettingsButton.style.display = 'block'
-	}
+  if (!settings) {
+    settingsForm.style.display = "block";
+  } else {
+    settingsForm.style.display = "none";
+    toggleSettingsButton.style.display = "block";
+  }
 
-	toggleSettingsButton.addEventListener('click', () => {
-		if (settingsForm.style.display === 'none') {
-			settingsForm.style.display = 'block'
-		} else {
-			settingsForm.style.display = 'none'
-		}
-	})
-})
+  toggleSettingsButton.addEventListener("click", () => {
+    if (settingsForm.style.display === "none") {
+      settingsForm.style.display = "block";
+    } else {
+      settingsForm.style.display = "none";
+    }
+  });
+});
 
-document.getElementById('settings-form').addEventListener('submit', (event) => {
-	event.preventDefault()
-	lat = document.getElementById('latitude').value
-	lon = document.getElementById('longitude').value
-	units = document.getElementById('units').value
-	const timeFormat = document.getElementById('time-format').value
-	twelveHour = timeFormat === '12'
+document.getElementById("settings-form").addEventListener("submit", (event) => {
+  event.preventDefault();
+  lat = document.getElementById("latitude").value;
+  lon = document.getElementById("longitude").value;
+  units = document.getElementById("units").value;
+  const timeFormat = document.getElementById("time-format").value;
+  twelveHour = timeFormat === "12";
 
-	localStorage.setItem(
-		'settings',
-		JSON.stringify({ lat, lon, units, timeFormat })
-	)
-	document.getElementById('settings-form').style.display = 'none'
-	document.getElementById('toggle-settings').style.display = 'block'
-	localStorage.removeItem('weatherData')
-})
+  localStorage.setItem(
+    "settings",
+    JSON.stringify({ lat, lon, units, timeFormat }),
+  );
+  document.getElementById("settings-form").style.display = "none";
+  document.getElementById("toggle-settings").style.display = "block";
+  localStorage.removeItem("weatherData");
+});
 
-const settings = JSON.parse(localStorage.getItem('settings')) || {
-	lat: '0',
-	lon: '0',
-	units: 'fahrenheit',
-	timeFormat: '12',
-}
+const settings = JSON.parse(localStorage.getItem("settings")) || {
+  lat: "0",
+  lon: "0",
+  units: "fahrenheit",
+  timeFormat: "12",
+};
 
-let lat = settings.lat
-let lon = settings.lon
-let units = settings.units
-let twelveHour = settings.timeFormat === '12'
+let lat = settings.lat;
+let lon = settings.lon;
+let units = settings.units;
+let twelveHour = settings.timeFormat === "12";
 
-let prevSec = -1
-let isFetching = false
+let prevSec = -1;
+let isFetching = false;
 
 async function updateClock() {
-	const now = new Date()
-	const currSec = now.getSeconds()
+  const now = new Date();
+  const currSec = now.getSeconds();
 
-	if (currSec !== prevSec) {
-		prevSec = currSec
+  if (currSec !== prevSec) {
+    prevSec = currSec;
 
-		let hours = now.getHours()
-		const ampm = hours >= 12 ? 'pm' : 'am'
-		if (twelveHour) {
-			hours = hours % 12 || 12
-		}
+    let hours = now.getHours();
+    const ampm = hours >= 12 ? "pm" : "am";
+    if (twelveHour) {
+      hours = hours % 12 || 12;
+    }
 
-		const minutes = now.getMinutes().toString().padStart(2, '0')
-		const seconds = currSec.toString().padStart(2, '0')
+    const minutes = now.getMinutes().toString().padStart(2, "0");
+    const seconds = currSec.toString().padStart(2, "0");
 
-		document.querySelector('.time').textContent = `${hours}:${minutes}`
-		document.querySelector('.sec').textContent = seconds
-		document.querySelector('.ampm').textContent = ampm
-		document.querySelector('.date').textContent = `${
-			now.getMonth() + 1
-		}.${now.getDate()}`
-		document.querySelector('.day').textContent = now
-			.toLocaleDateString('en-US', { weekday: 'long' })
-			.toLocaleLowerCase()
+    document.querySelector(".time").textContent = `${hours}:${minutes}`;
+    document.querySelector(".sec").textContent = seconds;
+    document.querySelector(".ampm").textContent = ampm;
+    document.querySelector(".date").textContent = `${
+      now.getMonth() + 1
+    }.${now.getDate()}`;
+    document.querySelector(".day").textContent = now
+      .toLocaleDateString("en-US", { weekday: "long" })
+      .toLocaleLowerCase();
 
-		checkWeather(now)
-	}
+    checkWeather(now);
+  }
 
-	requestAnimationFrame(updateClock)
+  requestAnimationFrame(updateClock);
 }
 
 function checkWeather(now) {
-	const cachedData = localStorage.getItem('weatherData')
-	if (cachedData) {
-		const data = JSON.parse(cachedData)
-		const cachedTime = new Date(data.current.time)
+  const cachedData = localStorage.getItem("weatherData");
+  if (cachedData) {
+    const data = JSON.parse(cachedData);
+    const cachedTime = new Date(data.current.time);
 
-		if (now - cachedTime < (15 * 60 + 10) * 1000) {
-			updateWeather(data)
-			return
-		}
-	}
+    if (now - cachedTime < (15 * 60 + 10) * 1000) {
+      updateWeather(data);
+      return;
+    }
+  }
 
-	if (!isFetching) {
-		fetchAPI()
-	}
+  if (!isFetching) {
+    fetchAPI();
+  }
 }
 
 async function fetchAPI() {
-	console.log(lat, lon, units)
-	try {
-		isFetching = true
-		const response = await fetch(
-			`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current=temperature_2m,weather_code&temperature_unit=${units}`
-		)
-		const data = await response.json()
-		console.log('successful fetch', data)
-		localStorage.setItem('weatherData', JSON.stringify(data))
-		updateWeather(data)
-		isFetching = false
-	} catch (error) {
-		console.error('error fetching weather data:', error)
-		isFetching = false
-	}
+  console.log(lat, lon, units);
+  try {
+    isFetching = true;
+    const response = await fetch(
+      `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current=temperature_2m,weather_code&temperature_unit=${units}`,
+    );
+    const data = await response.json();
+    console.log("successful fetch", data);
+    localStorage.setItem("weatherData", JSON.stringify(data));
+    updateWeather(data);
+    isFetching = false;
+  } catch (error) {
+    console.error("error fetching weather data:", error);
+    isFetching = false;
+  }
 }
 
 function updateWeather(data) {
-	const temperature = Math.round(data.current.temperature_2m)
-	const weatherCode = data.current.weather_code
-	const now = new Date()
-	const isDaytime = now.getHours() >= 6 && now.getHours() < 18
+  const temperature = Math.round(data.current.temperature_2m);
+  const weatherCode = data.current.weather_code;
+  const now = new Date();
+  const isDaytime = now.getHours() >= 6 && now.getHours() < 18;
 
-	document.querySelector('.temp').textContent = `${temperature}°`
-	document.querySelector('.weather').textContent =
-		weatherCodes[weatherCode][
-			isDaytime ? 'dayDescription' : 'nightDescription'
-		].toLocaleLowerCase()
+  document.querySelector(".temp").textContent = `${temperature}°`;
+  document.querySelector(".weather").textContent =
+    weatherCodes[weatherCode][
+      isDaytime ? "dayDescription" : "nightDescription"
+    ].toLocaleLowerCase();
 }
 
-updateClock()
+updateClock();
--- weatherCodes.js
+++ weatherCodes.js
@@ -1,114 +1,114 @@
 export const weatherCodes = {
-	0: {
-		dayDescription: 'Sunny',
-		nightDescription: 'Clear',
-	},
-	1: {
-		dayDescription: 'Sunny',
-		nightDescription: 'Clear',
-	},
-	2: {
-		dayDescription: 'Partly Cloudy',
-		nightDescription: 'Partly Cloudy',
-	},
-	3: {
-		dayDescription: 'Cloudy',
-		nightDescription: 'Cloudy',
-	},
-	45: {
-		dayDescription: 'Foggy',
-		nightDescription: 'Foggy',
-	},
-	48: {
-		dayDescription: 'Foggy',
-		nightDescription: 'Foggy',
-	},
-	51: {
-		dayDescription: 'L Drizzle',
-		nightDescription: 'L Drizzle',
-	},
-	53: {
-		dayDescription: 'Drizzle',
-		nightDescription: 'Drizzle',
-	},
-	55: {
-		dayDescription: 'H Drizzle',
-		nightDescription: 'H Drizzle',
-	},
-	56: {
-		dayDescription: 'L Drizzle',
-		nightDescription: 'L Drizzle',
-	},
-	57: {
-		dayDescription: 'Drizzle',
-		nightDescription: 'Drizzle',
-	},
-	61: {
-		dayDescription: 'L Rain',
-		nightDescription: 'L Rain',
-	},
-	63: {
-		dayDescription: 'Rain',
-		nightDescription: 'Rain',
-	},
-	65: {
-		dayDescription: 'H Rain',
-		nightDescription: 'H Rain',
-	},
-	66: {
-		dayDescription: 'L Rain',
-		nightDescription: 'L Rain',
-	},
-	67: {
-		dayDescription: 'Rain',
-		nightDescription: 'Rain',
-	},
-	71: {
-		dayDescription: 'L Snow',
-		nightDescription: 'L Snow',
-	},
-	73: {
-		dayDescription: 'Snow',
-		nightDescription: 'Snow',
-	},
-	75: {
-		dayDescription: 'H Snow',
-		nightDescription: 'H Snow',
-	},
-	77: {
-		dayDescription: 'Snow Grains',
-		nightDescription: 'Snow Grains',
-	},
-	80: {
-		dayDescription: 'L Showers',
-		nightDescription: 'L Showers',
-	},
-	81: {
-		dayDescription: 'Showers',
-		nightDescription: 'Showers',
-	},
-	82: {
-		dayDescription: 'H Showers',
-		nightDescription: 'H Showers',
-	},
-	85: {
-		dayDescription: 'L Snow Showers',
-		nightDescription: 'L Snow Showers',
-	},
-	86: {
-		dayDescription: 'Snow Showers',
-		nightDescription: 'Snow Showers',
-	},
-	95: {
-		dayDescription: 'Thunderstorm',
-		nightDescription: 'Thunderstorm',
-	},
-	96: {
-		dayDescription: 'L Hailstorm',
-		nightDescription: 'L Hailstorm',
-	},
-	99: {
-		dayDescription: 'Hailstorm',
-		nightDescription: 'Hailstorm',
-	},
-}
+  0: {
+    dayDescription: "Sunny",
+    nightDescription: "Clear",
+  },
+  1: {
+    dayDescription: "Sunny",
+    nightDescription: "Clear",
+  },
+  2: {
+    dayDescription: "Partly Cloudy",
+    nightDescription: "Partly Cloudy",
+  },
+  3: {
+    dayDescription: "Cloudy",
+    nightDescription: "Cloudy",
+  },
+  45: {
+    dayDescription: "Foggy",
+    nightDescription: "Foggy",
+  },
+  48: {
+    dayDescription: "Foggy",
+    nightDescription: "Foggy",
+  },
+  51: {
+    dayDescription: "L Drizzle",
+    nightDescription: "L Drizzle",
+  },
+  53: {
+    dayDescription: "Drizzle",
+    nightDescription: "Drizzle",
+  },
+  55: {
+    dayDescription: "H Drizzle",
+    nightDescription: "H Drizzle",
+  },
+  56: {
+    dayDescription: "L Drizzle",
+    nightDescription: "L Drizzle",
+  },
+  57: {
+    dayDescription: "Drizzle",
+    nightDescription: "Drizzle",
+  },
+  61: {
+    dayDescription: "L Rain",
+    nightDescription: "L Rain",
+  },
+  63: {
+    dayDescription: "Rain",
+    nightDescription: "Rain",
+  },
+  65: {
+    dayDescription: "H Rain",
+    nightDescription: "H Rain",
+  },
+  66: {
+    dayDescription: "L Rain",
+    nightDescription: "L Rain",
+  },
+  67: {
+    dayDescription: "Rain",
+    nightDescription: "Rain",
+  },
+  71: {
+    dayDescription: "L Snow",
+    nightDescription: "L Snow",
+  },
+  73: {
+    dayDescription: "Snow",
+    nightDescription: "Snow",
+  },
+  75: {
+    dayDescription: "H Snow",
+    nightDescription: "H Snow",
+  },
+  77: {
+    dayDescription: "Snow Grains",
+    nightDescription: "Snow Grains",
+  },
+  80: {
+    dayDescription: "L Showers",
+    nightDescription: "L Showers",
+  },
+  81: {
+    dayDescription: "Showers",
+    nightDescription: "Showers",
+  },
+  82: {
+    dayDescription: "H Showers",
+    nightDescription: "H Showers",
+  },
+  85: {
+    dayDescription: "L Snow Showers",
+    nightDescription: "L Snow Showers",
+  },
+  86: {
+    dayDescription: "Snow Showers",
+    nightDescription: "Snow Showers",
+  },
+  95: {
+    dayDescription: "Thunderstorm",
+    nightDescription: "Thunderstorm",
+  },
+  96: {
+    dayDescription: "L Hailstorm",
+    nightDescription: "L Hailstorm",
+  },
+  99: {
+    dayDescription: "Hailstorm",
+    nightDescription: "Hailstorm",
+  },
+};
